75f30e576fb1e0f3dced1a5340cbe15eaf7a15f8
ttplinh <ttphuonglinh.dt@gmail.com>
2025-03-27 17:34:03 +0700

    Add files via upload
    upload sample data

--- main.py
+++ main.py
@@ -1,6 +1,10 @@
 from data.service import *
+from backtesting.backtesting import *
+from config.config import optimization_params
 
 # Instantiate the DataService.
 data_service = DataService()
-print(data_service.get_data('2023-01-01', '2023-12-31'))
-
+train = pd.read_csv('train.csv')
+backtester = Backtesting()
+results = backtester.run(trading_data=train, params=optimization_params, asset_value=10000)
+print(results)
