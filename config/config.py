4050b23c5b9c4bdcdd71b1701e27962a4045472c
ttplinh <ttphuonglinh.dt@gmail.com>
2025-03-27 17:31:03 +0700

    Add files via upload
    update optimization param

--- config/config.py
+++ config/config.py
@@ -1,3 +1,5 @@
+import json
+
 DB_PARAMS = {
     "host": "api.algotrade.vn",
     "port": 5432,
@@ -5,3 +7,16 @@ DB_PARAMS = {
     "user": "intern_read_only",
     "password": "ZmDaLzFf8pg5"
 }
+
+
+VN30 = {
+    "auth_type": "Bearer",
+    "consumerID": "a00f2198360744158186e27b77002bcf",
+    "consumerSecret": "e6f7687210b44c6fa4f049c3dde81f75",
+    "url": "https://fc-data.ssi.com.vn/",
+    "stream_url": "https://fc-datahub.ssi.com.vn/"
+}
+
+optimization_params = {}
+with open("optimization/best_params.json", "r") as of:
+    optimization_params = json.load(of)
