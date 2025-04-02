f04a64477d2c01e5438b19feea984ee5cefd8d45
ttplinh <ttphuonglinh.dt@gmail.com>
2025-04-02 15:13:42 +0700

    Add files via upload

--- main.py
+++ main.py
@@ -1,10 +1,37 @@
 from data.service import *
 from backtesting.backtesting import *
 from config.config import optimization_params
+from optimization.optimization import *
+from performance.result import BacktestResult
+from performance.metric import Metric
+import pandas as pd
 
-# Instantiate the DataService.
+# Load the data
 data_service = DataService()
-train = pd.read_csv('train.csv')
-backtester = Backtesting()
-results = backtester.run(trading_data=train, params=optimization_params, asset_value=10000)
-print(results)
+train_data = data_service.get_train_data()
+test_data = data_service.get_test_data()
+
+# Print the head of the data
+print("Train Data:")
+print(train_data.head())
+print("Test Data:")
+print(test_data.head())
+
+# Run the backtest with the best parameters
+result = BacktestResult(optimization_params)
+
+# In-sample backtest
+insample_result = result.backtest_insample_data()
+print("Insample Backtest Result:")
+print(insample_result.head())
+metrics = Metric(insample_result)
+metrics.show_metrics()
+metrics.plot_pnl()
+
+# Out-of-sample backtest
+outsample_result = result.backtest_outsample_data()
+print("Out-of-sample Backtest Result:")
+print(outsample_result.head())
+metrics = Metric(outsample_result)
+metrics.show_metrics()
+metrics.plot_pnl()
