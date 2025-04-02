d918f71ff8499c3d95b4bac82f2d1d62005a5407
ttplinh <ttphuonglinh.dt@gmail.com>
2025-04-02 15:07:01 +0700

    Add files via upload

--- config/config.py
+++ config/config.py
@@ -11,8 +11,8 @@ DB_PARAMS = {
 
 VN30 = {
     "auth_type": "Bearer",
-    "consumerID": "a00f2198360744158186e27b77002bcf",
-    "consumerSecret": "e6f7687210b44c6fa4f049c3dde81f75",
+    "consumerID": "0",
+    "consumerSecret": "0",
     "url": "https://fc-data.ssi.com.vn/",
     "stream_url": "https://fc-datahub.ssi.com.vn/"
 }
